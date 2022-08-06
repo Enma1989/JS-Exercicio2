be8060a9355b7764fe0b66a73ff26eca670e8515
Enmanuel Perdomo <108840912+Enma1989@users.noreply.github.com>
2022-08-06 18:33:08 -0300

    Meu primeiro commit

---
+++ Exercício 2/script.js
@@ -0,0 +1,13 @@
+var nome = prompt('escreva seu nome');
+var profissão = prompt('inserir sua profissão');
+var exper = prompt('inserir anos de experiencia');
+var nome2 = prompt('escreva seu nome');
+var profissão2 = prompt('inserir sua profissão');
+var exper2 = prompt('inserir anos de experiencia');
+
+
+if (exper > exper2) {
+    console.log(`${nome} tem mas experiencia`);
+} else {
+    console.log(`${nome2} tem mas experiencia`);
+}
